31aa4c5d20f2c067f31a2623cb9d62bf7f312110
dehadeaaryan <dehadeaaryan@gmail.com>
2022-03-01 00:56:30 -0600

    resize image

--- src/pygame_builder/main.py
+++ src/pygame_builder/main.py
@@ -1,8 +1,16 @@
 import os
 import sys, pygame
 
+from PIL import Image
+import requests
+
+imgUrl = "https://raw.githubusercontent.com/dehadeaaryan/pygame-builder/main/pygame-builder.png"
+img = Image.open(requests.get(imgUrl, stream=True).raw)
+raw = img.tobytes("raw", 'RGBA')
+image = pygame.image.fromstring(raw, [1920, 1080], 'RGBA')
+
 class Pygame:
-    def __init__(self, backgroundImage, size : tuple or list, windowCaption = "Pygame", fps = 60, backgroundColour = (0, 0, 0), backgroundSound = None):
+    def __init__(self, backgroundImage = img, size = [1920, 1080], windowCaption = "Pygame", fps = 60, backgroundColour = (0, 0, 0), backgroundSound = None):
         
         os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
         
